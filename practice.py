ff7a6c24c845648511731cf6e2a2ce197e1ffb28
Scholes423 <78220332+Scholes423@users.noreply.github.com>
2021-02-01 22:44:50 +0800

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+#action1:求2+4+6+8+...+100的求和，用Python该如何写
+print("结果为：", sum(range(2, 101, 2)))
+
+# action2 统计全班的成绩
+# 班里有5名同学，现在需要你用Python来统计下这些人在语文、英语、数学中的平均成绩、最小成绩、最大成绩、方差、标准差。
+scores = pd.read_excel("scores.xlsx", encoding="utf-8")
+print(scores.describe().loc[["mean", "std", "min", "max"]])
+# 然后把这些人的总成绩排序，得出名次进行成绩输出（可以用numpy或pandas）
+scores["总成绩"] = scores[["语文", "数学", "英语"]].sum(axis=1)
+scores = scores.sort_values("总成绩", ascending=False)
+scores["排名"] = np.arange(1, scores.shape[0]+1)
+scores.set_index("姓名", inplace=True)
+print(scores)
+
+#Action3: 对汽车质量数据进行统计
+#数据集：car_complain.csv
+#600条汽车质量投诉
+#Step1，数据加载
+dfc = pd.read_csv("car_complain.csv", encoding="utf-8")
+len_rows = dfc.shape[0]
+#Step2，数据预处理
+dfc["brand"] = dfc["brand"].apply(lambda x:x.replace("-", ""))
+#拆分problem类型 => 多个字段,统计单行抱怨中所含的问题点个数
+new = dfc["problem"].str.get_dummies(",")
+dfc = pd.merge(dfc, new, how="outer", left_index=True, right_index=True)
+dfc["prob_count"] = dfc.iloc[:, dfc.columns.get_loc("status") + 1:].sum(axis=1)
+#Step3，数据统计
+#对数据进行探索：品牌投诉总数，车型投诉总数
+print(dfc.groupby("brand")["prob_count"].sum().sort_values(ascending=False))
+print(dfc.groupby("car_model")["prob_count"].sum().sort_values(ascending=False))
+#哪个品牌的平均车型投诉最多
+print((dfc.groupby("brand")["prob_count"].sum()/dfc.groupby("brand")["car_model"].agg(set).map(len)).sort_values(ascending=False))
+
+
+
+
+
+
